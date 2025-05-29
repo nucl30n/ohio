1b4a60d6183f24981f0e98805a099a5d99f8b52b
nucl30n <trigon297@gmail.com>
2025-05-29 14:31:25 -0400

    Update heroBan.lua

--- heroBan.lua
+++ heroBan.lua
@@ -1,3 +1,2 @@
 -- ServerScriptService/CmdrCommands/heroBan.lua
-local banApi = require(game.ServerScriptService.Modules.BanApi)
-return banApi.getCmdDefs("Hero")
+return require(game.ServerScriptService.Modules.banApi)("Hero")
