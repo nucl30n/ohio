0856220b8e92b5e564b6e3dc9939dc2b2616a2f9
nucl30n <trigon297@gmail.com>
2025-05-29 13:00:47 -0400

    Update adminBan.lua

--- adminBan.lua
+++ adminBan.lua
@@ -1,9 +1,3 @@
 -- ServerScriptService/CmdrCommands/adminBan.lua
-
-local BanUtils = require(game.ServerScriptService.Modules.BanApi)
-local group = "Admin"
-local cmdDefs = BanUtils.getCmdDefs(group)
-
-cmdDefs.Run = BanUtils.runBanCommand(group)
-
-return cmdDefs
+local banApi = require(game.ServerScriptService.Modules.BanApi)
+return banApi.getCmdDefs("Admin")
