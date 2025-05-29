95810a96453062562cdcd6dc9e300a1a650139c5
nucl30n <trigon297@gmail.com>
2025-05-29 12:32:41 -0400

    Update adminBan.lua

--- adminBan.lua
+++ adminBan.lua
@@ -1,25 +1,9 @@
--- Roblox BanAPI Cmdr def for "Admin" group
 -- ServerScriptService/CmdrCommands/adminBan.lua
 
-local BanUtils = require(game.ServerScriptService.Modules.BanUtils)
+local BanUtils = require(game.ServerScriptService.Modules.BanApi)
 local group = "Admin"
 local cmdDefs = BanUtils.getCmdDefs(group)
 
-cmdDefs.Run = function(context, target, reason, duration)
-    duration = duration:lower()
-    local Players = game:GetService("Players")
-    if not Players.BanAsync then return "BanAsync not available" end
-    if not BanUtils[context.Group][duration] then
-        return "Invalid duration: " .. BanUtils.getStr(context.Group)
-    else
-        Players:BanAsync(BanUtils.getBanCmd(context.Executor.Name, target, BanUtils[context.Group][duration], reason))
-    end
-
-    if target and target:IsDescendantOf(Players) then
-        target:Kick(BanUtils.getKickMsg(reason, duration))
-    end
-
-    return BanUtils.getFinalMsg(target, reason, duration)
-end
+cmdDefs.Run = BanUtils.runBanCommand(group)
 
 return cmdDefs
