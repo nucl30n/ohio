ba852530a76943a3dccbcb4357385c92cb190bd7
nucl30n <trigon297@gmail.com>
2025-05-30 14:55:10 -0400

    Update adminBan.lua

--- Cmdr/adminBan.lua
+++ Cmdr/adminBan.lua
@@ -1,2 +1,2 @@
 -- ServerScriptService/CmdrCommands/adminBan.lua
-return require(game.ServerScriptService.Modules.banApi)("Admin")
+return require(game.ServerScriptService.Modules.banApi).getCmdrDef("Admin")
